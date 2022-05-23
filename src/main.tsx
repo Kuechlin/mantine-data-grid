df3d1527a1dc5bf414d3172251b9317824a18c9d
Yannick Küchlin <y.kuechlin@Taifun-tofu.de>
2022-05-23 15:51:36 +0200

    ui improved

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,30 @@
-import { MantineProvider } from '@mantine/core';
+import {
+    Anchor,
+    Button,
+    Center,
+    MantineProvider,
+    Stack,
+    Title,
+} from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { dateColumn, numberColumn, stringColumn } from './components/columns';
 import DataGrid from './components/DataGrid';
 import { faker } from '@faker-js/faker';
+import { BrandGithub } from 'tabler-icons-react';
 
 type Data = {
     text: string;
+    animal: string;
+    city: string;
     value: number;
     date: Date;
 };
 
 var data: Data[] = new Array(10000).fill({}).map((i) => ({
     text: faker.name.findName(),
+    animal: faker.animal.cat(),
+    city: faker.address.city(),
     value: faker.datatype.number(),
     date: faker.datatype.datetime(),
 }));
@@ -24,34 +36,55 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             withGlobalStyles
             withNormalizeCSS
         >
-            <div
-                style={{
-                    height: '500px',
-                    width: '500px',
-                    margin: '100px auto',
-                }}
-            >
-                <DataGrid
-                    data={data}
-                    columns={[
-                        stringColumn<Data>({
-                            value: (d) => d.text,
-                            width: 100,
-                            label: 'Full Name ',
-                        }),
-                        numberColumn<Data>({
-                            value: (d) => d.value,
-                            width: 200,
-                            label: 'Value',
-                        }),
-                        dateColumn<Data>({
-                            value: (d) => d.date,
-                            label: 'Date',
-                            width: 200,
-                        }),
-                    ]}
-                />
-            </div>
+            <Stack>
+                <Center>
+                    <Title>Mantine Data Grid</Title>
+                </Center>
+                <Center>
+                    <Button
+                        leftIcon={<BrandGithub />}
+                        component="a"
+                        href="https://github.com/Kuechlin/mantine-data-grid"
+                        target="_blank"
+                        color="gray"
+                        children="Github"
+                    />
+                </Center>
+                <div
+                    style={{
+                        height: '500px',
+                        width: '500px',
+                        margin: 'auto',
+                    }}
+                >
+                    <DataGrid
+                        data={data}
+                        columns={[
+                            stringColumn<Data>({
+                                value: (d) => d.text,
+                                label: 'Full Name ',
+                            }),
+                            stringColumn<Data>({
+                                value: (d) => d.animal,
+                                label: 'Animal',
+                            }),
+                            stringColumn<Data>({
+                                value: (d) => d.city,
+                                label: 'City',
+                            }),
+                            numberColumn<Data>({
+                                value: (d) => d.value,
+                                label: 'Value',
+                            }),
+                            dateColumn<Data>({
+                                value: (d) => d.date,
+                                label: 'Date',
+                                width: 200,
+                            }),
+                        ]}
+                    />
+                </div>
+            </Stack>
         </MantineProvider>
     </React.StrictMode>
 );
